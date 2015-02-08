726b26d32cd3db1e1db22e01ae0dd713d51e256b
Kunal Nagpal <kunagpal@gmail.com>
2015-02-08 09:02:51 +0530

    More front-end progress, layout made more responsive.

--- public/scripts/developers.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Kunal Nagpal <kunagpal@gmail.com> on 03-02-2015.
- */
--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,24 @@ var async = require('async');
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
 var router = require('express').Router();
+var uri = process.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/project';
+var mongo = require('mongodb').MongoClient;
+var User;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next)
+{
+    if (req.signedCookies.name)
+    {
+        res.redirect('/');
+    }
+    else
+    {
+        res.render('index', { title: 'Express' });
+    }
 });
 // GET login page
 router.get('/login', function(req, res, next) {
     res.render('login', { title: 'Express' });
-});
-// GET registration page
-router.get('/register', function(req, res, next) {
-    res.render('register', { title: 'Express' });
 });
 // GET developers page
 router.get('/developers', function(req, res, next) {
@@ -26,6 +33,107 @@ router.get('/rules', function(req, res, next) {
 router.get('/forgot', function(req, res, next) {
     res.render('forgot', { title: 'Express' });
 });
+// GET reset token page
+router.get('/reset/:token', function(req, res) {
+    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+            req.flash('error', 'Password reset token is invalid or has expired.');
+            return res.redirect('/forgot');
+        }
+        res.render('reset', {
+            user: req.user,
+            title: 'Title'
+        });
+    });
+});
+// GET solved example
+router.get('/solved', function(req, res, next) {
+    res.render('solved', { title: 'Express' });
+});
+// GET leaderboard
+router.get('/leader', function(req, res, next) {
+    res.render('leader', { title: 'Express' });
+});
+// GET game page
+router.get('/play', function(req, res, next) {
+    res.render('game', { title: 'Express' });
+});
+// POST login page
+router.post('/login', function(req, res, next) {
+    res.render('login', { title: 'Express' });
+});
+// GET registration page
+router.get('/register', function(req, res, next) {
+    if (req.signedCookies.name)
+    {
+        res.redirect('/home');
+    }
+    else
+    {
+        res.render('register', { response: "" });
+    }
+});
+// POST register page
+router.post('/register', function(req, res, next) {
+    var onGetCount =
+    mongoUsers.getCount(function (err, number)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Reached");
+            if (req.body.password === req.body.confirm)
+            {
+                var newUser =
+                {
+                    _id : req.body.manager_name,
+                    dob : new Date(),
+                    team_no : parseInt(number) + 1,
+                    password_hash : bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10)),
+                    email : req.body.email,
+                    phone : req.body.phone,
+                    win : 0,
+                    loss : 0,
+                    tied : 0,
+                    played : 0,
+                    points : 0,
+                    ratio : 0.0,
+                    form : 1,
+                    streak: 0,
+                    worst : -1,
+                    best : Number.MAX_VALUE,
+                    avg : 0
+                };
+                var onInsert = function (err, docs)
+                {
+                    if (err)
+                    {
+                        console.log(err.message);
+                        // Make it more user friendly, output the error to the view
+                        res.render('register', {response: "Team Name Already Exists"});
+                    }
+                    else
+                    {
+                        var name = docs[0]['_id'];
+                        res.cookie('name', name, {maxAge: 86400000, signed: true});
+                        res.redirect('/home/players');
+                    }
+                };
+                mongoUsers.insert(newUser, onInsert);
+            }
+            else
+            {
+                console.log("Incorrect Password");
+                res.render('register', {response: "Passwords do not match"});
+            }
+        }
+    }
+    )
+});
+// POST forgot password page
 router.post('/forgot', function(req, res, next) {
     async.waterfall([
         function(done) {
@@ -35,15 +143,14 @@ router.post('/forgot', function(req, res, next) {
             });
         },
         function(token, done) {
+
             User.findOne({ email: req.body.email }, function(err, user) {
                 if (!user) {
                     req.flash('error', 'No account with that email address exists.');
                     return res.redirect('/forgot');
                 }
-
                 user.resetPasswordToken = token;
                 user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
                 user.save(function(err) {
                     done(err, token, user);
                 });
@@ -53,13 +160,13 @@ router.post('/forgot', function(req, res, next) {
             var smtpTransport = nodemailer.createTransport('SMTP', {
                 service: 'Gmail',
                 auth: {
-                    user: 'kunagpal@gmail.com',
-                    pass: 'kunal398nagpal'
+                    user: 'sudokuchampster@gmail.com',
+                    pass: process.env.PASSWORD
                 }
             });
             var mailOptions = {
                 to: user.email,
-                from: 'kunagpal@gmail.com',
+                from: 'sudokuchampster@gmail.com',
                 subject: 'Node.js Password Reset',
                 text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
                 'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
@@ -76,30 +183,7 @@ router.post('/forgot', function(req, res, next) {
         res.redirect('/forgot');
     });
 });
-// GET solved example
-router.get('/solved', function(req, res, next) {
-    res.render('solved', { title: 'Express' });
-});
-// GET leaderboard
-router.get('/leader', function(req, res, next) {
-    res.render('leader', { title: 'Express' });
-});
-// GET game page
-router.get('/play', function(req, res, next) {
-    res.render('game', { title: 'Express' });
-});
-router.get('/reset/:token', function(req, res) {
-    User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
-        if (!user) {
-            req.flash('error', 'Password reset token is invalid or has expired.');
-            return res.redirect('/forgot');
-        }
-        res.render('reset', {
-            user: req.user,
-            title: 'Title'
-        });
-    });
-});
+// POST reset token page
 router.post('/reset/:token', function(req, res) {
     async.waterfall([
         function(done) {
@@ -108,11 +192,9 @@ router.post('/reset/:token', function(req, res) {
                     req.flash('error', 'Password reset token is invalid or has expired.');
                     return res.redirect('back');
                 }
-
                 user.password = req.body.password;
                 user.resetPasswordToken = undefined;
                 user.resetPasswordExpires = undefined;
-
                 user.save(function(err) {
                     req.logIn(user, function(err) {
                         done(err, user);
@@ -124,13 +206,13 @@ router.post('/reset/:token', function(req, res) {
             var smtpTransport = nodemailer.createTransport('SMTP', {
                 service: 'Gmail',
                 auth: {
-                    user: 'kunagpal@gmail.com',
-                    pass: 'kunal398nagpal'
+                    user: 'sudokuchampster@gmail.com',
+                    pass: process.env.PASSWORD
                 }
             });
             var mailOptions = {
                 to: user.email,
-                from: 'kunagpal@gmail.com',
+                from: 'sudokuchampster@gmail.com',
                 subject: 'Your password has been changed',
                 text: 'Hello,\n\n' +
                 'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
@@ -143,5 +225,17 @@ router.post('/reset/:token', function(req, res) {
     ], function(err) {
         res.redirect('/');
     });
+});
+// POST logout page
+router.post('/logout', function(req, res, next) {
+    if (req.signedCookies.name)
+    {
+        res.clearCookie('name', { });
+        res.redirect('/logout');
+    }
+    else
+    {
+        res.redirect('/');
+    }
 });
 module.exports = router;
