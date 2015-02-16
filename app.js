8124e9d8bf6d0faca64cdefa676b3045ce6df082
Kunal Nagpal <kunagpal@gmail.com>
2015-02-16 06:42:57 +0530

    Production check.

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser("secret"));
-app.use(session({ secret : 'session secret key'}));
+//app.use(session({ secret : 'session secret key'}));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
